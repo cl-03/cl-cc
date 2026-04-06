228ec29bea65bbe9c4d9c3c810253334f3ea6480
cl-03 <cl-03@users.noreply.github.com>
2026-04-06 17:37:52 +0800

    feat: expand protected repository metadata directories to .git, .hg, .svn

--- src/tools/repository-metadata-guard.lisp
+++ src/tools/repository-metadata-guard.lisp
@@ -1,9 +1,10 @@
 ;;;; src/tools/repository-metadata-guard.lisp - repository metadata path guard helpers
 (in-package :cl-cc.tools)
 
+
 (defparameter +protected-repository-metadata-directories+
-  '(".git")
-  "禁止写入的受保护仓库元数据目录段。")
+  '(".git" ".hg" ".svn")
+  "禁止写入的受保护仓库元数据目录段。默认包含 .git、.hg、.svn。")
 
 (defun %protected-repository-metadata-path-p (path)
   (let ((components (cl-ppcre:split "[/\\\\]+" (or path ""))))
--- tests/unit/repository-metadata-guard-test.lisp
+++ tests/unit/repository-metadata-guard-test.lisp
@@ -6,10 +6,24 @@
 (in-suite repository-metadata-guard-test)
 
 (test protected-repository-metadata-path-p-matches-repository-metadata-directories-only
+  ;; .git
   (is (cl-cc.tools::%protected-repository-metadata-path-p ".git/config"))
   (is (cl-cc.tools::%protected-repository-metadata-path-p "repo/.git/hooks/pre-commit"))
   (is (cl-cc.tools::%protected-repository-metadata-path-p "repo\\.git\\config"))
   (is (cl-cc.tools::%protected-repository-metadata-path-p "./.git/config"))
+  ;; .hg
+  (is (cl-cc.tools::%protected-repository-metadata-path-p ".hg/store/data"))
+  (is (cl-cc.tools::%protected-repository-metadata-path-p "repo/.hg/00changelog.i"))
+  (is (cl-cc.tools::%protected-repository-metadata-path-p "repo\\.hg\\store"))
+  (is (cl-cc.tools::%protected-repository-metadata-path-p "./.hg/requires"))
+  ;; .svn
+  (is (cl-cc.tools::%protected-repository-metadata-path-p ".svn/wc.db"))
+  (is (cl-cc.tools::%protected-repository-metadata-path-p "repo/.svn/pristine/"))
+  (is (cl-cc.tools::%protected-repository-metadata-path-p "repo\\.svn\\entries"))
+  (is (cl-cc.tools::%protected-repository-metadata-path-p "./.svn/format"))
+  ;; 非元数据
   (is (not (cl-cc.tools::%protected-repository-metadata-path-p ".gitignore")))
   (is (not (cl-cc.tools::%protected-repository-metadata-path-p ".github/workflows/ci.yml")))
-  (is (not (cl-cc.tools::%protected-repository-metadata-path-p "docs/about.git.txt"))))
+  (is (not (cl-cc.tools::%protected-repository-metadata-path-p "docs/about.git.txt")))
+  (is (not (cl-cc.tools::%protected-repository-metadata-path-p ".hgignore")))
+  (is (not (cl-cc.tools::%protected-repository-metadata-path-p ".svnignore"))))
